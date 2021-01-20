c0b4e712ffb6cc12d8af328b29f7c97214620cb3
MinaTurgut <minaturgut24@gmail.com>
2021-01-20 23:26:53 +0300

    Add project files.

---
+++ TekrarEdenharfler/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TekrarEdenharfler
+{
+    class Program
+    {
+        
+        public static char Tekrarsiz(string ilkchar)
+        {
+            int i, j;
+            bool tekrarEden;
+            char[] charlar = ilkchar.ToCharArray();
+            for (i = 0; i < ilkchar.Length; i++)
+            {
+                tekrarEden = false;
+                for(j= 0; j < ilkchar.Length; j++)
+                {
+                    if ((i != j) && ilkchar[i] == ilkchar[j]);
+                    tekrarEden = true;
+                    break;
+                }
+                if(tekrarEden == false)
+                {
+                    return charlar[i];
+                }
+            }
+            return default(char);
+        }
+
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir kelime giriniz: ");
+            string kelime = Console.ReadLine();
+            char ilkharf = Tekrarsiz(kelime);
+            Console.WriteLine("Sonuç: ", ilkharf);
+            Console.ReadKey();
+            
+            
+            
+            
+
+        }
+    }
+}
